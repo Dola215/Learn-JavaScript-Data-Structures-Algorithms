a302f94c39dc1393a3b05877e4f4899fe681ec79
Dola215 <adelyasseromer215@gmail.com>
2024-10-09 19:32:59 +0300

    create a Node & LL class and add the basic methold

---
+++ index.js
@@ -0,0 +1,28 @@
+// create Node Class
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = next;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  printList() {}
+  push(value) {}
+  pop() {}
+  unshift(value) {}
+  shift() {}
+  get() {}
+  set(index, value) {}
+  insert(index, value) {}
+
+  getHead() {}
+  getTail() {}
+  getLength() {}
+  makeEmpty() {}
+}
